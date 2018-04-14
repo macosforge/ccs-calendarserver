4ce13d58e18b9fe42452f896934772d97182a649
Giorgio Azzinnaro <giorgio.azzinnaro@gmail.com>
2018-04-14 19:25:58 +0200

    refactoring entrypoint sh

--- contrib/docker/docker_cmd.sh
+++ contrib/docker/docker_cmd.sh
@@ -2,22 +2,26 @@
 
 # This is because OpenShift runs with random UIDs,
 # and ccs expects the UID to be in /etc/passwd
+PASSWD_TEMP_FILE="/home/ccs/contrib/docker/passwd.template"
+PASSWD_FILE="/tmp/passwd"
+
 export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
-envsubst < "/home/ccs/contrib/docker/passwd.template" > /tmp/passwd
+envsubst < $PASSWD_TEMP_FILE > $PASSWD_FILE
+
 export LD_PRELOAD=/usr/lib/libnss_wrapper.so
-export NSS_WRAPPER_PASSWD=/tmp/passwd
+export NSS_WRAPPER_PASSWD=$PASSWD_FILE
 export NSS_WRAPPER_GROUP=/etc/group
 
 # Just get our conf file
-TEMP_FILE="/home/ccs/contrib/docker/caldavd.plist.template"
+CCS_CONF_TEMP_FILE="/home/ccs/contrib/docker/caldavd.plist.template"
 
 # It is important that this dir is world-writable,
 # /tmp usually is
-CONF_FILE="/tmp/caldavd.plist"
+CCS_CONF_FILE="/tmp/caldavd.plist"
 
 # Replace any env variable as they come from docker run
-envsubst < $TEMP_FILE > $CONF_FILE
+envsubst < $CCS_CONF_TEMP_FILE > $CCS_CONF_FILE
 
 # Run caldavd, no daemonize, log to stdout
-caldavd -X -L -f $CONF_FILE
+caldavd -X -L -f $CCS_CONF_FILE
